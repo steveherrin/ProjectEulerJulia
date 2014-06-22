4b016f5967ba12efb20d04e1bdb069d1cd929882
Steve Herrin <steve.herrin@gmail.com>
2014-06-22 09:13:36 -0700

    Have these use the consolidated primes code.

--- 007/007.jl
+++ 007/007.jl
@@ -1,23 +1,6 @@
 using Base.Test
 
-function primes_up_to(n::Integer)
-    crossed_out = falses(n)
-    primes = Int[]
-    for i in 2:n
-        if crossed_out[i]
-            continue
-        end
-        push!(primes, i)
-        for j in 2*i:i:n
-            crossed_out[j] = true
-        end
-    end
-    return primes
-end
-
-@test primes_up_to(2) == [2]
-@test primes_up_to(4) == [2, 3]
-@test primes_up_to(10) == [2, 3, 5, 7]
+include("../primes.jl")
 
 function get_Nth_prime(N::Integer)
     up_to = N
