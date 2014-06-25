5a84ae65545f7abfb2e078b7b21db3aebe3f2ee6
Steve Herrin <sherrin@23andme.com>
2014-06-25 13:09:30 -0700

    BigDecimal now should handle negative numbers and subtraction properly. Add tests I've thought of.

--- big_decimal.jl
+++ big_decimal.jl
@@ -9,6 +9,22 @@ type BigDecimal <: Integer
     negative::Bool
 end
 
+function nines_complement(digits::Vector{Uint8}, desired_length=0)
+    complement = Uint8[9 - digit for digit in digits]
+    while length(complement) < desired_length
+        push!(complement, 9)
+    end
+    return complement
+end
+
+function remove_trailing_zeros(digits::Vector{Uint8})
+    digits_copy = digits
+    while length(digits_copy) > 1 && digits_copy[end] == 0
+        pop!(digits_copy)
+    end
+    return digits_copy
+end
+
 function BigDecimal(x::Integer)
     digits = Uint8[]
     negative = x < 0
@@ -17,6 +33,10 @@ function BigDecimal(x::Integer)
         push!(digits, x % 10)
         x = div(x, 10)
     end
+    if length(digits) == 0
+        push!(digits, 0)
+    end
+    digits = remove_trailing_zeros(digits)
     return BigDecimal(digits, negative)
 end
 
@@ -30,33 +50,52 @@ function BigDecimal(x::String)
     for i in length(x):-1:start
         push!(digits, parseint(x[i:i]))
     end
+    digits = remove_trailing_zeros(digits)
     return BigDecimal(digits, negative)
 end
 
 function dec(x::BigDecimal)
-    s = reduce(*, map(dec, reverse(x.digits)))
     if x.negative
-        s = "-" * s
+        s = "-"
+    else
+        s = ""
+    end
+    if length(x.digits) == 1
+        s *= dec(x.digits[1])
+    else
+        s *= reduce(*, map(dec, reverse(x.digits)))
     end
     return s
 end
 string(x::BigDecimal) = dec(x)
 show(io::IO, x::BigDecimal) = print(io, string(x))
 
+function -(x::BigDecimal)
+    negative = !x.negative
+    return BigDecimal(x.digits, negative)
+end
+
 function abs(x::BigDecimal)
-    return BigDecimal(x.digits, false)
+    if !x.negative
+        return x
+    else
+        return -x
+    end
 end
 
 function +(x::BigDecimal, y::BigDecimal)
+    if abs(y) > abs(x)
+        # simplifies the logic
+        return y + x
+    end
+    if !x.negative && y.negative
+        return x - abs(y)
+    end
     digits = Uint8[]
-    N = max(length(x.digits), length(y.digits))
+    N = length(x.digits)
     carry = 0
     for i in 1:N
-        if i > length(x.digits)
-            a = 0
-        else
-            a = x.digits[i]
-        end
+        a = x.digits[i]
         if i > length(y.digits)
             b = 0
         else
@@ -70,17 +109,36 @@ function +(x::BigDecimal, y::BigDecimal)
     if carry > 0
         push!(digits, carry)
     end
-    result = BigDecimal(digits, x.negative)
-    return result
+    return BigDecimal(digits, x.negative)
+end
+
+function -(x::BigDecimal, y::BigDecimal)
+    if abs(x) < abs(y)
+        # simplifies the logic
+	return -(y - x)
+    end
+    if !x.negative && y.negative
+        return x + abs(y)
+    elseif x.negative && !y.negative
+        return x + (-y)
+    end
+    y_nc = BigDecimal(nines_complement(y.digits, length(x.digits)), x.negative)
+    difference = x + y_nc
+    difference += BigDecimal(1)
+    pop!(difference.digits)  # remove extra 1
+    difference.digits = remove_trailing_zeros(difference.digits)
+    return difference
 end
 
 function *(x::BigDecimal, y::BigDecimal)
     product = BigDecimal(0)
-    count = BigDecimal(0)
-    while count < y
-        product += x
-        count += BigDecimal(1)
+    count = abs(y)
+    abs_x = abs(x)
+    while count > BigDecimal(0)
+        product += abs(x)
+        count -= BigDecimal(1)
     end
+    product.negative = x.negative $ y.negative
     return product
 end
 
@@ -97,6 +155,9 @@ end
 
 
 function <(x::BigDecimal, y::BigDecimal)
+    if x.negative && !y.negative
+        return true
+    end
     if length(x.digits) < length(y.digits)
         return true
     elseif length(x.digits) == length(y.digits)
@@ -128,11 +189,20 @@ function test_BigDecimal()
     @test a <= b
     @test a <= b + BigDecimal(1)
     @test a < b + BigDecimal(1)
+    @test -(-b) == b
+    @test -a < b
+    @test -a == -b
     @test !(BigDecimal("211") < BigDecimal("112"))
     @test BigDecimal("112") < BigDecimal("211")
     @test a + b == BigDecimal(reverse(Uint8[2,4,6,9,1,2]), false)
-    @test a + b == BigDecimal(reverse(Uint8[2,4,6,9,1,2]), false)
+    @test a - b == BigDecimal(0)
+    @test a - BigDecimal(1) == BigDecimal(123455)
+    @test (-a) - BigDecimal(1) == BigDecimal(-123457)
+    @test BigDecimal(1) - a == BigDecimal(-123455)
     @test a * b == BigDecimal("15241383936")
+    @test a * (-b) == BigDecimal("-15241383936")
+    @test (-a) * (-b) == BigDecimal("15241383936")
+    @test (-a) * b == -BigDecimal("15241383936")
     @test factorial(BigDecimal(4)) == BigDecimal(24)
     return true
 end
